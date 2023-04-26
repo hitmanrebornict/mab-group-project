d7c6e35238fe0bf4f0d5db02682fd1467725a9e9
DingHeng <jayleedingheng0401@gmail.com>
2023-04-27 00:47:50 +0800

    feat: activity_setup_for_record_meal.xml modified

--- app/src/main/java/com/example/mab_group_project/ExpandableListViewAdapter.java
+++ app/src/main/java/com/example/mab_group_project/ExpandableListViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mab_group_project;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
